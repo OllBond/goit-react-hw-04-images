f12e32484c1a13c0706bfc04d7ebfeb3ac125769
Olena <newsroommy@gmail.com>
2023-02-05 03:00:25 +0300

    update

--- src/modules/ImageSearch/Searchbar/Searchbar.jsx
+++ src/modules/ImageSearch/Searchbar/Searchbar.jsx
@@ -1,24 +1,15 @@
 import PropTypes from 'prop-types';
 import { BsSearch } from 'react-icons/bs';
-import { useState } from 'react';
 import initialState from './initialState';
+import useForm from 'shared/components/Modal/hooks/useForm';
 import css from './Searchbar.module.css';
 
 const Searchbar = ({ onSubmit }) => {
-  const [state, setState] = useState({ ...initialState });
-
-  // target - це деструктуризація event
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
-    setState(prevState => {
-      return { ...prevState, [name]: value };
-    });
-  };
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit({ ...state });
-    setState({ ...initialState });
-  };
+  // кастомний хук
+  const { state, handleChange, handleSubmit } = useForm({
+    initialState,
+    onSubmit,
+  });
   const { search } = state;
   return (
     <header className={css.searchbar}>
